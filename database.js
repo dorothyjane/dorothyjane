a3d58b0d0be0c61cbc3d8c3790968be29bf2e1e4
Dorothy Wingrove <wingrove.d@gmail.com>
2015-08-22 22:26:04 +0100

    switch to passing in entire db environment

--- database.js
+++ database.js
@@ -1,7 +1,5 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host :     process.env.DATABASE_URL,
-});
+var connection = mysql.createConnection(process.env.DATABASE_URL);
 
 module.exports = connection;
