4ee210173ba3833c9c91168506fa71272cdc592f
Dorothy Wingrove <wingrove.d@gmail.com>
2015-08-22 23:06:46 +0100

    remove subtitle

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ app.get('/', function(request, response, next){
   //response.status(200).send("</h1>Welcome to DorothyJanes site.</h1><br/> It is currently under construction. Please come back soon.");
   response.render('index', {
       title: 'dorothy jane wingrove',
-      subtitle: 'web developer',
       links: {
         twitter: "https://twitter.com/_dorothyjane",
         instagram: "http://instagram.com/dottiejane/",
