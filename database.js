a0333b282be37ab02544ae00f91371901f5def3e
Dorothy Wingrove <wingrove.d@gmail.com>
2015-10-07 21:20:39 +0100

    test using host for getting db

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ var db_url = URL.parse(process.env.DATABASE_URL);
 var auth = db_url.auth.split(':');
 
 var connection = mysql.createPool({
-  host            : db_url.hostname,
+  host            : db_url.host,
   user            : auth[0],
   password        : auth[1],
   database        : db_url.path.substr(1)
