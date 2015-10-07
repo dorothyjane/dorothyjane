c55b0772be262cda719469440a350d7e3c316704
Dorothy Wingrove <wingrove.d@gmail.com>
2015-10-07 20:50:52 +0100

    fix db connection error

--- database.js
+++ database.js
@@ -1,5 +1,17 @@
 var mysql = require('mysql');
+var URL = require('url');
 
-var connection = mysql.createPool(process.env.DATABASE_URL);
+
+var db_url = URL.parse(process.env.DATABASE_URL);
+var auth = db_url.auth.split(':');
+
+var connection = mysql.createPool({
+  host            : db_url.hostname,
+  user            : auth[0],
+  password        : auth[1],
+  database        : db_url.path.substr(1)
+});
+
+
 
 module.exports = connection;
