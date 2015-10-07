b342699460b424b05aad228b312b112fe3e27fdd
Dorothy Wingrove <wingrove.d@gmail.com>
2015-10-07 21:26:53 +0100

    fix db - just parse a stringgit add database.js

--- database.js
+++ database.js
@@ -2,16 +2,17 @@ var mysql = require('mysql');
 var URL = require('url');
 
 
-var db_url = URL.parse(process.env.DATABASE_URL);
-var auth = db_url.auth.split(':');
+// var db_url = URL.parse(process.env.DATABASE_URL);
+// var auth = db_url.auth.split(':');
 
-var connection = mysql.createPool({
-  host            : db_url.host,
-  user            : auth[0],
-  password        : auth[1],
-  database        : db_url.path.substr(1)
-});
+// var connection = mysql.createPool({
+//   host            : db_url.hostname,
+//   user            : auth[0],
+//   password        : auth[1],
+//   database        : db_url.path.substr(1)
+// });
 
+var connection = mysql.createPool(process.env.DATABASE_URL);
 
 
 module.exports = connection;
