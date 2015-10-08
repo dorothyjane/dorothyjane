448551ffe7ff577ac5eb68d9d580b554ae12245d
Dorothy Wingrove <wingrove.d@gmail.com>
2015-10-08 13:36:24 +0100

    working mouse trail - with 4 trail balls

--- assets/javascripts/mouse-trail.js
+++ assets/javascripts/mouse-trail.js
@@ -1,24 +1,16 @@
 
 var trailElements = document.getElementsByClassName('trailElement');
-
-var mouseX = 100;
-var mouseY = 100;
-var dx = [];
-var dy = [];
-var vx = [0,0,0,0];
-var vy = [0,0,0,0];
-var ease = [0.05,2,0.01,0.8];
-var friction = [0.95, 0.2, 0.5, 0.5];
-
+var mouse = { 'x': 100, 'y': 100};
+var drag = [0.35,0.3,0.25,0.2];
 
 function updateMouseLocations(e){
-  mouseX = e.pageX;
-  mouseY = e.pageY;
+  mouse['x'] = e.pageX;
+  mouse['y'] = e.pageY;
 }
 
 function trailTheElements(){
   console.log('trail');
-  for(x=0; x < trailElements.length; x++){
+  for(var x=0; x < trailElements.length; x++){
     console.log('count', x);
     // trailElements[x].style.left = e.pageX + 'px';
     // trailElements[x].style.top = e.pageY + 'px';
@@ -32,23 +24,31 @@ function loop(trailElement, i){
     x = parseFloat(trailElement.style.left) || 0;
     y = parseFloat(trailElement.style.top) || 0;
 
-    x_diff = mouseX - x;
-    y_diff = mouseY - y;
 
-    x_move = x_diff * 0.5;
-    y_move = y_diff * 0.5;
-    // dx[i] = mouseX - x;
-    // dy[i] = mouseY - y;
-    // vx[i] += dx[i]*ease[i];
-    // vy[i] += dy[i]*ease[i];
-    // vx[i] *= friction[i];
-    // vy[i] *= friction[i];
-    // x += vx[i];
-    // y += vy[i];
-    console.log('x before', x)
+    if(typeof trailElements[i-1] != "undefined"){
+      if(i == 1){
+      }
+      following_x = parseFloat(trailElements[i-1].style.left);
+      following_y = parseFloat(trailElements[i-1].style.top);
+      console.log(i, following_x, following_y);
+    } else {
+      following_x =  mouse['x'];
+      following_y = mouse['y'];
+    }
+
+
+    x_diff = following_x - x;
+    y_diff = following_y - y;
+
+    x_diff = x_diff > 0 ? (x_diff - 10) : (x_diff + 10)
+    y_diff = y_diff > 0 ? (y_diff - 10) : (y_diff + 10)
+
+    x_move = x_diff * drag[i];
+    y_move = y_diff * drag[i];
+
     x += x_move
     y += y_move
-    console.log('x after', x, x_move, i);
+
     trailElement.style.left = x + 'px';
     trailElement.style.top = y + 'px';
     // loop(trailElement, i);
