ff3bd655555fae78e60b08afe76e9b404a43ffe8
dorothy wingrove <dorothy@dorothys-MBP-2.lan>
2016-07-09 22:33:41 +0100

    add quill wysiwyg to blog in admin and make put requests works

--- app.js
+++ app.js
@@ -32,7 +32,17 @@ app.use(session({
 
 // body parse my params into json
 app.use( bodyParser.urlencoded({ extended: false}) );
-app.use(methodOverride('_method'))
+// app.use(methodOverride('_method'))
+
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 // check for sesssion if go to place where need session
 app.all('/admin/*', function(req, res, next){
